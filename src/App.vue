49b19bc6bbe4c3189ed3027994b2753a441ebab1
obedm503 <obedm503@gmail.com>
2017-12-14 20:45:58 -0600

    from quasar to plain vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
-  <!-- Don't drop "q-app" class -->
-  <div id="q-app">
-    <router-view></router-view>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from 'vue';
+
+import HelloWorld from './components/HelloWorld.vue';
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+});
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
